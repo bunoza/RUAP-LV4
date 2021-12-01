53edc67194f2715a609836cc13fbd171d6de9048
bunoza <domagoj.bunoza@gmail.com>
2021-12-01 18:28:49 +0100

    add item to cart and remove it test

---
+++ RUAP_LV4/RUAP_LV4/UnitTest5.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using Assert = NUnit.Framework.Assert;
+
+namespace RUAP_LV4
+{
+    [TestClass]
+    public class UnitTest5
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/digital-downloads");
+            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[4]/div[2]/div[2]/div[2]/div[3]/div[1]/div/div[1]/a/img")).Click();
+            driver.FindElement(By.Id("addtocart_53_EnteredQuantity")).Click();
+            driver.FindElement(By.Id("addtocart_53_EnteredQuantity")).Clear();
+            driver.FindElement(By.Id("addtocart_53_EnteredQuantity")).SendKeys("2");
+            driver.FindElement(By.Id("add-to-cart-button-53")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id=\"topcartlink\"]/a")).Click();
+            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div/form/table/tbody/tr/td[1]/input")).Click();
+            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div/form/div[1]/div/input[1]")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
